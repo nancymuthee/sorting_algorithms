af06a7b49887cc494141326e4be8ff0912864178
briankod <brianmugambi199@gmail.com>
2022-07-19 12:53:57 +0300

    Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -21,11 +21,12 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void swap_next(listint_t *node, listint_t **list);
+void swap_previous(listint_t *node, listint_t **list);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
