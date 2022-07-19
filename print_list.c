b07f3b9633d79f7a4c77f1f642b12fcc1eac5143
briankod <brianmugambi199@gmail.com>
2022-07-19 17:26:53 +0300

    Sorting algorithms & Big O

--- print_list.c
+++ print_list.c
@@ -8,20 +8,6 @@
  */
 void print_list(const listint_t *list)
 {
-<<<<<<< HEAD
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-=======
 	int i;
 
 	i = 0;
@@ -34,5 +20,4 @@ void print_list(const listint_t *list)
 		list = list->next;
 	}
 	printf("\n");
->>>>>>> af06a7b49887cc494141326e4be8ff0912864178
 }
