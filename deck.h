f4124e5052f787c55424510d103f51d8d2e05f40
briankod <brianmugambi199@gmail.com>
2022-07-20 02:51:40 +0300

    update header file

--- deck.h
+++ deck.h
@@ -25,8 +25,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-		const char *value;
-			const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -38,9 +38,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-		const card_t *card;
-			struct deck_node_s *prev;
-				struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
